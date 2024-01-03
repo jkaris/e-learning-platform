5b05c309d2f192847ea9470b1ea295bcf77c173e
Joseph Kariuki <joehene@gmail.com>
2024-01-03 10:19:47 +0300

    Formatted code with Black

--- ed_system/api_examples/enroll_all.py
+++ ed_system/api_examples/enroll_all.py
@@ -1,20 +1,22 @@
 import requests
 
-username = 'your_username'
-password = 'your_password'
-base_url = 'http://127.0.0.1:8000/api/'
+username = "your_username"
+password = "your_password"
+base_url = "http://127.0.0.1:8000/api/"
 
 # Retrieve all courses
-r = requests.get(f'{base_url}courses/')
+r = requests.get(f"{base_url}courses/")
 courses = r.json()
-available_courses = ', '.join([course['title'] for course in courses])
-print(f'Available courses: {available_courses}')
+available_courses = ", ".join([course["title"] for course in courses])
+print(f"Available courses: {available_courses}")
 
 for course in courses:
-    course_id = course['id']
-    course_title = course['title']
-    r = requests.post(f'{base_url}courses/{course_id}/enroll/', auth=(username, password))
+    course_id = course["id"]
+    course_title = course["title"]
+    r = requests.post(
+        f"{base_url}courses/{course_id}/enroll/", auth=(username, password)
+    )
 
     if r.status_code == 200:
         # Successful request
-        print(f'Successfully enrolled in {course_title}')
+        print(f"Successfully enrolled in {course_title}")
